7d20161548e295e51ad488dbdc8a47fde9ac62b5
Emanuel Araújo <emanuel.qa@gmail.com>
2016-08-10 01:55:18 -0300

    add mdl in the screen the login

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,3 @@
 class HomesController < ApplicationController
   
-  def index
-    @resource ||= User.new
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-
-end
+ end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
+	def resource
+		@resource ||= User.new	
+	end
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]	
+	end
+    
 
 end
